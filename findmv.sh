95ce41f506ffbba871b9c8bf62ca0bf824c56b56
jeanron100 <yangjianrong1985@163.com>
2018-03-13 22:17:10 +0800

    Create findmv.sh
    find materialized views general information

---
+++ findmv.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+VIEW_OWNER=`sqlplus -silent $DB_CONN_STR@$SH_DB_SID <<END
+set pagesize 50 feedback off verify off heading on echo off
+col owner format a20
+col mview_name format a30
+set linesize 150
+select owner, mview_name,query_len,updatable,rewrite_enabled,refresh_mode,refresh_method,build_mode,last_refresh_type,
+last_refresh_date,compile_state
+  from dba_mviews 
+  where mview_name like upper('%'||'$2'||'%') and owner=upper('$1') ;
+exit;
+END`
+
+if [ -z "$VIEW_OWNER" ]; then
+ echo "no object exists, please check again"
+ exit 0
+else
+ echo '*******************************************'
+ echo " $VIEW_OWNER    "
+ echo '*******************************************'
+fi
